e644e82044f3f48e1b1ac18778994e56bbd81f63
ElTurroDeLaRapier <dieguitosus@hotmail.com>
2024-12-02 20:53:00 -0300

    Initial scaffolding for web app

---
+++ src/main/java/com/directa24/directa24_back_end_dev_challenge/Directa24BackEndDevChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.directa24.directa24_back_end_dev_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Directa24BackEndDevChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Directa24BackEndDevChallengeApplication.class, args);
+	}
+
+}
